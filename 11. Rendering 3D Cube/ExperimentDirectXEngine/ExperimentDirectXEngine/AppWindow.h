3f487ce09bb7bd44245b2304cdc1d6d97558ac61
Sevdat <55320710+Sevdat@users.noreply.github.com>
2023-11-11 12:12:34 +0300

    Add files via upload

---
+++ 11. Rendering 3D Cube/ExperimentDirectXEngine/ExperimentDirectXEngine/AppWindow.h
@@ -0,0 +1,45 @@
+//Readme: This project is licensed under the terms of the MIT license.
+//Author: Pardcode
+
+
+#pragma once
+#include "Window.h"
+#include "GraphicsEngine.h"
+#include "SwapChain.h"
+#include "DeviceContext.h"
+#include "VertexBuffer.h"
+#include "IndexBuffer.h"
+#include "ConstantBuffer.h"
+#include "VertexShader.h"
+#include "PixelShader.h"
+
+class AppWindow: public Window
+{
+public:
+	AppWindow();
+
+    void updateQuadPosition();
+
+	~AppWindow();
+
+	// Inherited via Window
+	void onCreate() override;
+	void onUpdate() override;
+	void onDestroy() override;
+private:
+	SwapChain* m_swap_chain;
+	VertexBuffer* m_vb;
+    VertexShader* m_vs;
+    PixelShader* m_ps;
+    ConstantBuffer* m_cb;
+    IndexBuffer* m_ib;
+private:
+    float m_old_delta;
+    float m_new_delta;
+    float m_delta_time;
+
+    float m_delta_pos;
+    float m_delta_scale;
+
+};
+
